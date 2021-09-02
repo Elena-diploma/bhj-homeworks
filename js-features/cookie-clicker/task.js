bbf52fb7cd3ca76123b07ebdb9d300a76ef4c9f5
Elena Tursunova <elenatursunova@gmail.com>
2021-09-02 20:38:33 +0300

    с исправлениями 2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,7 +2,6 @@ const clicker__counter = document.getElementById('clicker__counter');
 const cookie = document.getElementById('cookie');
 const clickerSpeed = document.getElementById('clicker__speed');
 let lastClickTime = Date.now();
-let allClickTime = 0;
 let clicks = 0;
 
 cookie.onclick = clicker;
@@ -10,9 +9,8 @@ function clicker () {
     clicks++;
     clicker__counter.textContent = clicks.toString();
     if(lastClickTime !== 0) {
-        const delta = new Date() - lastClickTime;
-        allClickTime = allClickTime + delta/1000;
-        clickerSpeed.textContent = (1/allClickTime).toFixed(2);
+        const delta = (Date.now() - lastClickTime)/1000;
+        clickerSpeed.textContent = (1/delta).toFixed(2);
     }
     lastClickTime  = new Date();
 
