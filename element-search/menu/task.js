da5610841e969b18060c7c2dca9cd5bd78624c98
Elena Tursunova <elenatursunova@gmail.com>
2021-09-06 16:11:26 +0300

    версия 3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,15 +1,15 @@
-const menuClick = document.querySelectorAll("li.menu__item");
+const menuClick = document.querySelectorAll("ul.menu_main > li.menu__item > a");
 const menuMain = document.querySelectorAll("ul.menu_sub");
 
 menuClick.forEach((el) => el.onclick = function() {
-    const currentSubMenu = el.querySelector("ul.menu_sub");
-
+    const currentSubMenu = el.parentNode.querySelector("ul.menu_sub");
     if(currentSubMenu != null && currentSubMenu.classList.contains("menu_active")) {
         currentSubMenu.classList.remove("menu_active");
-
     } else {
         menuMain.forEach((item) => item.classList.remove("menu_active"));
         if(currentSubMenu != null ) currentSubMenu.classList.toggle("menu_active");
+    }
+    if(currentSubMenu != null) {
         return false;
     }
 });
